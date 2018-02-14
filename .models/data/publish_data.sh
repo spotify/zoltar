ab49c39c67386ac06fee9e08264daf55bea219d8
Fallon Chen <fallon@spotify.com>
2018-02-13 19:17:37 -0500

    wip

--- .models/data/publish_data.sh
+++ .models/data/publish_data.sh
@@ -10,4 +10,4 @@ TEMP_DIR=$(mktemp -d)
 wget --directory-prefix=$TEMP_DIR https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data 
 sed '$d' "$TEMP_DIR/iris.data" > "$TEMP_DIR/iris_clean.data"
 
-bq load $DEST_TABLE "$TEMP_DIR/iris_clean.data" sepal_length_cm:float,sepal_width_cm:float,petal_length_cm:float,petal_width_cm:float,class:string
+bq load $DEST_TABLE "$TEMP_DIR/iris_clean.data" sepal_length:float,sepal_width:float,petal_length:float,petal_width:float,class_name:string
---
+++ .models/features/project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")

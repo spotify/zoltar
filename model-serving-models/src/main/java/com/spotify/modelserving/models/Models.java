cdf4f760cf72f98efbdd355d627ea68b53077837
Filipe Regadas <regadas@spotify.com>
2018-02-28 16:07:05 -0500

    Add tensorflow model load options

--- model-serving-models/src/main/java/com/spotify/modelserving/models/Models.java
+++ model-serving-models/src/main/java/com/spotify/modelserving/models/Models.java
@@ -19,4 +19,13 @@ public final class Models {
                                            final FeatureSpec<T> featureSpec) throws IOException {
     return TensorFlowModel.create(URI.create(modelUri), URI.create(settingsUri), featureSpec);
   }
+
+  static <T> TensorFlowModel<T> tensorFlow(final String modelUri,
+                                           final String settingsUri,
+                                           final FeatureSpec<T> featureSpec,
+                                           final TensorFlowModel.Options options)
+      throws IOException {
+    return TensorFlowModel
+        .create(URI.create(modelUri), URI.create(settingsUri), featureSpec, options);
+  }
 }
--- model-serving-tensorflow/src/main/java/com/spotify/modelserving/tf/TensorFlowModel.java
+++ model-serving-tensorflow/src/main/java/com/spotify/modelserving/tf/TensorFlowModel.java
@@ -19,40 +19,63 @@ package com.spotify.modelserving.tf;
 
 import static com.spotify.modelserving.fs.Resource.ReadFns.asString;
 
+import com.google.auto.value.AutoValue;
 import com.spotify.featran.FeatureSpec;
 import com.spotify.modelserving.Model;
 import com.spotify.modelserving.fs.Resource;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import org.tensorflow.SavedModelBundle;
 
 public class TensorFlowModel<T> implements Model<SavedModelBundle, T> {
 
+  private static final Options DEFAULT_OPTIONS = Options.builder()
+      .tags(Collections.singletonList("serve"))
+      .build();
+
   private final SavedModelBundle model;
   private final String settings;
   private final FeatureSpec<T> featureSpec;
+  private final Options options;
 
   public static <T> TensorFlowModel<T> create(String modelUri,
                                               String settingsUri,
                                               FeatureSpec<T> featureSpec) throws IOException {
-    return create(URI.create(modelUri), URI.create(settingsUri), featureSpec);
+    return create(modelUri, settingsUri, featureSpec, DEFAULT_OPTIONS);
+  }
+
+  public static <T> TensorFlowModel<T> create(String modelUri,
+                                              String settingsUri,
+                                              FeatureSpec<T> featureSpec,
+                                              Options options) throws IOException {
+    return create(URI.create(modelUri), URI.create(settingsUri), featureSpec, options);
   }
 
   public static <T> TensorFlowModel<T> create(URI modelResource,
                                               URI settingsResource,
                                               FeatureSpec<T> featureSpec) throws IOException {
+    return create(modelResource, settingsResource, featureSpec, DEFAULT_OPTIONS);
+  }
+
+  public static <T> TensorFlowModel<T> create(URI modelResource,
+                                              URI settingsResource,
+                                              FeatureSpec<T> featureSpec,
+                                              Options options) throws IOException {
     final String settings = Resource.from(settingsResource).read(asString());
-    return new TensorFlowModel<>(modelResource.toString(), settings, featureSpec);
+    return new TensorFlowModel<>(modelResource.toString(), settings, featureSpec, options);
   }
 
   private TensorFlowModel(String exportDir,
                           String settings,
-                          FeatureSpec<T> featureSpec) {
+                          FeatureSpec<T> featureSpec,
+                          Options options) {
     // TODO: copy saved model from remote FS, object stores etc to local filesystem
-    // tags come from: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/saved_model/tag_constants.py#L26
-    this.model = SavedModelBundle.load(exportDir, "serve");
+    this.model = SavedModelBundle.load(exportDir, options.tags().toArray(new String[0]));
     this.settings = settings;
     this.featureSpec = featureSpec;
+    this.options = options;
   }
 
   @Override
@@ -75,6 +98,29 @@ public class TensorFlowModel<T> implements Model<SavedModelBundle, T> {
   @Override
   public FeatureSpec<T> featureSpec() {
     return featureSpec;
+  }
+
+  public Options options() {
+    return options;
+  }
+
+  @AutoValue
+  public abstract static class Options {
+
+    // tags come from: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/saved_model/tag_constants.py#L26
+    public abstract List<String> tags();
+
+    public static Builder builder() {
+      return new AutoValue_TensorFlowModel_Options.Builder();
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+
+      public abstract Builder tags(List<String> tags);
+
+      public abstract Options build();
+    }
   }
 
 }

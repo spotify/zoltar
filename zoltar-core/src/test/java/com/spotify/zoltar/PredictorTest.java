7b9f0b8acec6f3cd1a1e0b2c0af5350f9fb3b2c3
Filipe Regadas <filiperegadas@gmail.com>
2018-03-26 16:45:47 -0400

    Add more predictor tests

--- zoltar-core/src/test/java/com/spotify/zoltar/PredictorTest.java
+++ zoltar-core/src/test/java/com/spotify/zoltar/PredictorTest.java
@@ -20,14 +20,23 @@
 
 package com.spotify.zoltar;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import com.spotify.zoltar.FeatureExtractFns.ExtractFn;
+import com.spotify.zoltar.FeatureExtractFns.SingleExtractFn;
+import com.spotify.zoltar.PredictFns.AsyncPredictFn;
+import com.spotify.zoltar.PredictFns.PredictFn;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class PredictorTest {
@@ -51,8 +60,8 @@ public class PredictorTest {
     final Duration predictionTimeout = Duration.ZERO;
 
     final ExtractFn<Object, Object> extractFn = inputs -> Collections.emptyList();
-    final PredictFns.PredictFn<DummyModel, Object, Object, Object> predictFn =
-        (model, vectors) -> null;
+    final PredictFn<DummyModel, Object, Object, Object> predictFn =
+        (model, vectors) -> Collections.emptyList();
 
     try {
       Predictor.create(new DummyModel(), extractFn, predictFn)
@@ -64,5 +73,38 @@ public class PredictorTest {
     } catch (Exception e) {
       assertTrue(e.getCause() instanceof TimeoutException);
     }
+  }
+
+  @Test
+  public void empty() throws InterruptedException, ExecutionException, TimeoutException {
+    final Duration wait = Duration.ofSeconds(1);
+    final ExtractFn<Object, Object> extractFn = inputs -> Collections.emptyList();
+    final AsyncPredictFn<DummyModel, Object, Object, Object> predictFn =
+        (model, vectors) -> CompletableFuture.completedFuture(Collections.emptyList());
+
+    Predictor.create(new DummyModel(), extractFn, predictFn)
+        .predict()
+        .toCompletableFuture()
+        .get(wait.toMillis(), TimeUnit.MILLISECONDS);
+  }
+
+  @Test
+  public void nonEmpty() throws InterruptedException, ExecutionException, TimeoutException {
+    final Duration wait = Duration.ofSeconds(1);
+    final SingleExtractFn<Integer, Float> extractFn = input -> (float) input / 10;
+    final PredictFn<DummyModel, Integer, Float, Float> predictFn = (model, vectors) -> {
+      return vectors.stream()
+          .map(vector -> Prediction.create(vector.input(), vector.value() * 2))
+          .collect(Collectors.toList());
+    };
+
+    final List<Prediction<Integer, Float>> predictions = Predictor
+        .create(new DummyModel(), extractFn, predictFn)
+        .predict(1)
+        .toCompletableFuture()
+        .get(wait.toMillis(), TimeUnit.MILLISECONDS);
+
+    assertThat(predictions.size(), is(1));
+    assertThat(predictions.get(0), is(Prediction.create(1, 0.2f)));
   }
 }

e5d32ddd41758c397e4e2ce36a3ef8f6e2a2b54d
Romain Yon <romain@spotify.com>
2018-04-02 16:21:50 +0100

    provide help to build simple tf predictors

--- examples/apollo-service-example/src/main/java/com/spotify/zoltar/examples/apollo/IrisPrediction.java
+++ examples/apollo-service-example/src/main/java/com/spotify/zoltar/examples/apollo/IrisPrediction.java
@@ -24,31 +24,17 @@ import com.google.common.collect.ImmutableMap;
 import com.spotify.apollo.Response;
 import com.spotify.apollo.Status;
 import com.spotify.featran.FeatureSpec;
-import com.spotify.futures.CompletableFutures;
-import com.spotify.zoltar.FeatureExtractFns.ExtractFn;
 import com.spotify.zoltar.IrisFeaturesSpec;
 import com.spotify.zoltar.IrisFeaturesSpec.Iris;
-import com.spotify.zoltar.ModelLoader;
-import com.spotify.zoltar.Models;
-import com.spotify.zoltar.Prediction;
 import com.spotify.zoltar.Predictor;
-import com.spotify.zoltar.featran.FeatranExtractFns;
 import com.spotify.zoltar.tf.JTensor;
-import com.spotify.zoltar.tf.TensorFlowExtras;
-import com.spotify.zoltar.tf.TensorFlowModel;
-import com.spotify.zoltar.tf.TensorFlowPredictFn;
+import com.spotify.zoltar.tf.SimpleTensorFlowPredictorBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.Tensors;
-import org.tensorflow.example.Example;
+import java.util.concurrent.CompletionStage;
 import scala.Option;
 
 /**
@@ -56,11 +42,11 @@ import scala.Option;
  */
 public class IrisPrediction {
 
-  private static Predictor<Iris, Long> predictor;
-  private static Map<Integer, String> idToClass = ImmutableMap.of(
-      0, "Iris-setosa",
-      1, "Iris-versicolor",
-      2, "Iris-virginica");
+  private static Predictor<Iris, JTensor> predictor;
+  private static Map<Long, String> idToClass = ImmutableMap.of(
+      0L, "Iris-setosa",
+      1L, "Iris-versicolor",
+      2L, "Iris-virginica");
 
   /**
    * Configure Iris prediction, should be called at the service startup/configuration stage.
@@ -71,27 +57,17 @@ public class IrisPrediction {
   public static void configure(final URI modelDirUri, final URI settingsUri) throws IOException {
     final FeatureSpec<Iris> irisFeatureSpec = IrisFeaturesSpec.irisFeaturesSpec();
     final String settings = new String(Files.readAllBytes(Paths.get(settingsUri)));
-    final ModelLoader<TensorFlowModel> modelLoader =
-        Models.tensorFlow(modelDirUri.toString());
 
-    final ExtractFn<Iris, Example> extractFn =
-        FeatranExtractFns.example(irisFeatureSpec, settings);
-
-    final TensorFlowPredictFn<Iris, Long> predictFn = (model, vectors) -> {
-      final List<CompletableFuture<Prediction<Iris, Long>>> predictions =
-          vectors.stream()
-              .map(vector -> CompletableFuture
-                  .supplyAsync(() -> predictFn(model, vector.value()))
-                  .thenApply(v -> Prediction.create(vector.input(), v)))
-              .collect(Collectors.toList());
-      return CompletableFutures.allAsList(predictions);
-    };
-
-    predictor = Predictor.create(modelLoader, extractFn, predictFn);
+    predictor = new SimpleTensorFlowPredictorBuilder<Iris>()
+        .withFeatranExtractFns(irisFeatureSpec, settings)
+        .withModelUri(modelDirUri.toString())
+        .withInputOp("input_example_tensor")
+        .withPredictOp("linear/head/predictions/class_ids")
+        .build();
   }
 
   /**
-   * Prediction endpoint. Takes a request in a from of a String containing iris features `-`
+   * Prediction endpoint. Takes a request in a form of a String containing iris features `-`
    * separated, and returns a response in a form of a predicted iris class.
    */
   public static Response<String> predict(final String requestFeatures) {
@@ -112,31 +88,19 @@ public class IrisPrediction {
         Option.apply(Double.parseDouble(features[3])),
         Option.empty());
 
-    int[] predictions = new int[0];
     try {
-      predictions = predictor
+      final CompletionStage<String> futurePrediction = predictor
           .predict(featureData)
           .thenApply(p -> p
               .stream()
-              .mapToInt(prediction -> prediction.value().intValue())
-              .toArray()).toCompletableFuture().get();
+              .mapToLong(prediction -> prediction.value().longValue()[0]).toArray())
+          .thenApply(p -> idToClass.get(p[0]));
+
+      return Response.forPayload(futurePrediction.toCompletableFuture().get());
     } catch (final Exception e) {
       e.printStackTrace();
-      //TODO: what to return in case of failure here?
+      return Response.forStatus(Status.INTERNAL_SERVER_ERROR);
     }
-    final String predictedClass = idToClass.get(predictions[0]);
-    return Response.forPayload(predictedClass);
   }
 
-  private static long predictFn(final TensorFlowModel model, final Example example) {
-    final byte[][] b = new byte[1][];
-    b[0] = example.toByteArray();
-    try (Tensor<String> t = Tensors.create(b)) {
-      final Session.Runner runner = model.instance().session().runner()
-              .feed("input_example_tensor", t);
-      final String op = "linear/head/predictions/class_ids";
-      final Map<String, JTensor> result = TensorFlowExtras.runAndExtract(runner, op);
-      return result.get(op).longValue()[0];
-    }
-  }
 }
---
+++ zoltar-tensorflow/src/main/java/com/spotify/zoltar/tf/SimpleTensorFlowPredictorBuilder.java
@@ -0,0 +1,134 @@
+/*-
+ * -\-\-
+ * zoltar-tensorflow
+ * --
+ * Copyright (C) 2016 - 2018 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.zoltar.tf;
+
+import com.spotify.featran.FeatureSpec;
+import com.spotify.futures.CompletableFutures;
+import com.spotify.zoltar.FeatureExtractFns;
+import com.spotify.zoltar.ModelLoader;
+import com.spotify.zoltar.Prediction;
+import com.spotify.zoltar.Predictor;
+import com.spotify.zoltar.featran.FeatranExtractFns;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.Tensors;
+import org.tensorflow.example.Example;
+
+/**
+ * Helps to build a simple TensorFlow predictor.
+ * In this context, a simple TensorFlow predictor has the following characteristics:
+ * - Has a single input op
+ * - Has a single output op
+ * - Uses Featran to extract and consume Example objects
+ * The output of the Predictor will always be a {@link JTensor}.
+ */
+public class SimpleTensorFlowPredictorBuilder<InputT> {
+
+
+  private FeatureExtractFns.ExtractFn<InputT, Example> extractFn;
+  private String inputOp;
+  private String predictOp;
+  private ModelLoader<TensorFlowModel> modelLoader;
+
+
+  /**
+   * Parameters for FeatranExtractFns.
+   *
+   * @param featureSpec Featran's {@link FeatureSpec}.
+   * @param settings    JSON settings from a previous session.
+   */
+  public SimpleTensorFlowPredictorBuilder<InputT> withFeatranExtractFns(
+      final FeatureSpec<InputT> featureSpec,
+      final String settings) {
+    extractFn = FeatranExtractFns.example(featureSpec, settings);
+    return this;
+  }
+
+  /**
+   * Name of the input op node in the TF graph.
+   */
+  public SimpleTensorFlowPredictorBuilder<InputT> withInputOp(final String inputOp) {
+    this.inputOp = inputOp;
+    return this;
+  }
+
+  /**
+   * Name of the predict op node in the TF graph.
+   */
+  public SimpleTensorFlowPredictorBuilder<InputT> withPredictOp(final String predictOp) {
+    this.predictOp = predictOp;
+    return this;
+  }
+
+  /**
+   * Loads a saved TensorFlow {@link org.tensorflow.SavedModelBundle}, can be a URI to a local
+   * filesystem, resource, GCS etc.
+   */
+  public SimpleTensorFlowPredictorBuilder<InputT> withModelUri(final String modelUri) {
+    modelLoader = TensorFlowLoader.create(modelUri);
+    return this;
+  }
+
+  private static JTensor predictFn(final TensorFlowModel model,
+                                   final Example example,
+                                   final String inputOp,
+                                   final String predictOp) {
+    final byte[][] b = new byte[1][];
+    b[0] = example.toByteArray();
+    try (Tensor<String> t = Tensors.create(b)) {
+      final Session.Runner runner = model.instance().session().runner()
+          .feed(inputOp, t);
+      final Map<String, JTensor> result = TensorFlowExtras.runAndExtract(runner, predictOp);
+      return result.get(predictOp);
+    }
+  }
+
+  /**
+   * Builds the Predictor.
+   */
+  public Predictor<InputT, JTensor> build() {
+    assert extractFn != null;
+    assert inputOp != null;
+    assert predictOp != null;
+    assert modelLoader != null;
+
+    final TensorFlowPredictFn<InputT, JTensor> predictFn = (model, vectors) -> {
+      final List<CompletableFuture<Prediction<InputT, JTensor>>> predictions =
+          vectors.stream()
+              .map(vector -> CompletableFuture
+                  .supplyAsync(() -> predictFn(model,
+                      vector.value(),
+                      inputOp,
+                      predictOp))
+                  .thenApply(v -> Prediction.create(vector.input(), v)))
+              .collect(Collectors.toList());
+      return CompletableFutures.allAsList(predictions);
+    };
+
+    return Predictor.create(modelLoader, extractFn, predictFn);
+
+  }
+}
+

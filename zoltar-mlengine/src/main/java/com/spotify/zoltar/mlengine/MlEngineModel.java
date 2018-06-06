c259a63951b04a440c70985fe4dea578ee63f66e
Filipe Regadas <filiperegadas@gmail.com>
2018-06-06 16:25:46 -0400

    test

--- zoltar-mlengine/src/main/java/com/spotify/zoltar/mlengine/MlEngineModel.java
+++ zoltar-mlengine/src/main/java/com/spotify/zoltar/mlengine/MlEngineModel.java
@@ -21,7 +21,6 @@
 package com.spotify.zoltar.mlengine;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.util.Utils;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -54,7 +53,7 @@ public abstract class MlEngineModel implements Model<CloudMachineLearningEngine>
    *           <pre>"projects/{PROJECT_ID}/models/{MODEL_ID}/versions/{MODEL_VERSION}"</pre>
    */
   public static MlEngineModel create(final Model.Id id) throws Exception {
-    final HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    final HttpTransport httpTransport = Utils.getDefaultTransport();
     final JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
     final GoogleCredential credential = GoogleCredential.getApplicationDefault();
 

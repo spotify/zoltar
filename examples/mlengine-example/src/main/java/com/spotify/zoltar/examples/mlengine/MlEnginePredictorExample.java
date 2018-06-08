c7038fcfabd05a103afefa15394b1ddcc660e934
Filipe Regadas <filiperegadas@gmail.com>
2018-06-08 12:38:02 -0400

    Google Cloud ML Engine support (#119)

---
+++ examples/mlengine-example/src/main/java/com/spotify/zoltar/examples/mlengine/MlEnginePredictorExample.java
@@ -0,0 +1,138 @@
+/*-
+ * -\-\-
+ * mlengine-example
+ * --
+ * Copyright (C) 2016 - 2018 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.zoltar.examples.mlengine;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+import com.spotify.featran.FeatureSpec;
+import com.spotify.futures.CompletableFutures;
+import com.spotify.zoltar.FeatureExtractFns.ExtractFn;
+import com.spotify.zoltar.IrisFeaturesSpec;
+import com.spotify.zoltar.IrisFeaturesSpec.Iris;
+import com.spotify.zoltar.Models;
+import com.spotify.zoltar.Prediction;
+import com.spotify.zoltar.Predictor;
+import com.spotify.zoltar.Predictors;
+import com.spotify.zoltar.featran.FeatranExtractFns;
+import com.spotify.zoltar.mlengine.MlEngineLoader;
+import com.spotify.zoltar.mlengine.MlEnginePredictException;
+import com.spotify.zoltar.mlengine.MlEnginePredictFn;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.tensorflow.example.Example;
+
+/**
+ * Cloud Machine Learning Engine online prediction example.
+ */
+public final class MlEnginePredictorExample implements Predictor<Iris, Integer> {
+
+  private final Predictor<Iris, Integer> predictor;
+
+  private MlEnginePredictorExample(final Predictor<Iris, Integer> predictor) {
+    this.predictor = predictor;
+  }
+
+  /**
+   * Runs a simple Iris prediction against the running Iris ml-engine instance.
+   */
+  public static MlEnginePredictorExample create(final String projectId,
+                                                final String modelId,
+                                                final String versionId) throws Exception {
+    final MlEngineLoader mlEngineLoader = Models.mlEngine(projectId, modelId, versionId);
+
+    final FeatureSpec<Iris> irisFeatureSpec = IrisFeaturesSpec.irisFeaturesSpec();
+    final URI settingsUri = MlEnginePredictorExample.class.getResource("/settings.json").toURI();
+    final String settings = new String(Files.readAllBytes(Paths.get(settingsUri)));
+    final ExtractFn<Iris, Example> extractFn =
+        FeatranExtractFns.example(irisFeatureSpec, settings);
+
+    final MlEnginePredictFn<Iris, Example, Integer> predictFn = (model, vectors) -> {
+      final List<CompletableFuture<Prediction<Iris, Integer>>> predictions =
+          vectors.stream()
+              .map(vector -> CompletableFuture
+                  .supplyAsync(() -> {
+                    try {
+                      final List<Example> input = Collections.singletonList(vector.value());
+                      final MlEnginePrediction result = model
+                          .predictExamples(input)
+                          .values(MlEnginePrediction.class)
+                          .get(0);
+
+                      final int max = IntStream.range(0, result.scores().size())
+                          .reduce((i, j) -> {
+                            final BigDecimal ci = result.scores().get(i);
+                            final BigDecimal cj = result.scores().get(j);
+                            return ci.compareTo(cj) > 0 ? i : j;
+                          })
+                          .getAsInt();
+
+                      return result.classes().get(max);
+                    } catch (IOException | MlEnginePredictException | ExecutionException e) {
+                      throw new RuntimeException(e);
+                    }
+                  })
+                  .thenApply(v -> Prediction.create(vector.input(), v)))
+              .collect(Collectors.toList());
+      return CompletableFutures.allAsList(predictions);
+    };
+
+    final Predictor<Iris, Integer> predictor =
+        Predictors.newBuilder(mlEngineLoader, extractFn, predictFn).predictor();
+
+    return new MlEnginePredictorExample(predictor);
+  }
+
+  @Override
+  public CompletionStage<List<Prediction<Iris, Integer>>> predict(
+      final ScheduledExecutorService scheduler,
+      final Duration timeout,
+      final Iris... input) {
+    return predictor.predict(scheduler, timeout, input);
+  }
+
+  @AutoValue
+  abstract static class MlEnginePrediction {
+
+    public abstract List<Integer> classes();
+
+    public abstract List<BigDecimal> scores();
+
+    @JsonCreator
+    static MlEnginePrediction create(@JsonProperty("classes") final List<Integer> classes,
+                                     @JsonProperty("scores") final List<BigDecimal> scores) {
+      return new AutoValue_MlEnginePredictorExample_MlEnginePrediction(classes, scores);
+    }
+  }
+}
+
---
+++ zoltar-mlengine/src/main/java/com/spotify/zoltar/mlengine/MlEngineModel.java
@@ -0,0 +1,196 @@
+/*-
+ * -\-\-
+ * mlengine-example
+ * --
+ * Copyright (C) 2016 - 2018 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.zoltar.mlengine;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.util.Utils;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.ml.v1.CloudMachineLearningEngine;
+import com.google.api.services.ml.v1.CloudMachineLearningEngineScopes;
+import com.google.api.services.ml.v1.model.GoogleApiHttpBody;
+import com.google.api.services.ml.v1.model.GoogleCloudMlV1PredictRequest;
+import com.google.auto.value.AutoValue;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.io.BaseEncoding;
+import com.spotify.zoltar.Model;
+import com.spotify.zoltar.mlengine.MlEngineModel.Response.Predictions;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import org.tensorflow.example.Example;
+
+/**
+ * This model can be used to allow prediction on models deployed to Google Cloud ML Engine.
+ * scikit-learn, XGBoost and TensorFlow models are supported.
+ */
+@AutoValue
+public abstract class MlEngineModel implements Model<CloudMachineLearningEngine> {
+
+  private static final String APPLICATION_NAME = "zoltar";
+
+  /**
+   * Creates a Google Cloud ML Engine backed model.
+   *
+   * @param id {@link Model.Id} needs to be created with the following format:
+   *           <pre>"projects/{PROJECT_ID}/models/{MODEL_ID}"</pre>
+   *           or
+   *           <pre>"projects/{PROJECT_ID}/models/{MODEL_ID}/versions/{MODEL_VERSION}"</pre>
+   */
+  public static MlEngineModel create(final Model.Id id)
+      throws IOException, GeneralSecurityException {
+    final HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    final JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
+    final GoogleCredential credential = GoogleCredential
+        .getApplicationDefault()
+        .createScoped(CloudMachineLearningEngineScopes.all());
+
+    final CloudMachineLearningEngine mlEngine = new CloudMachineLearningEngine
+        .Builder(httpTransport, jsonFactory, credential)
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+
+    return new AutoValue_MlEngineModel(id, mlEngine, httpTransport);
+  }
+
+  abstract HttpTransport httpTransport();
+
+  /**
+   * Predict.
+   *
+   * @param data prediction input data.
+   */
+  public Predictions predict(final List<?> data) throws IOException, MlEnginePredictException {
+    final GoogleCloudMlV1PredictRequest predict =
+        new GoogleCloudMlV1PredictRequest().set("instances", data);
+    final GoogleApiHttpBody httpBody = instance().projects()
+        .predict(id().value(), predict)
+        .execute();
+
+    final Response response = Response.from(httpBody);
+    return response.predictions().orElseThrow(() -> {
+      return response.error().map(MlEnginePredictException::new).get();
+    });
+  }
+
+  /**
+   * Predict.
+   *
+   * @param examples TensorFlow {@link Example} input data.
+   */
+  public Predictions predictExamples(final List<Example> examples)
+      throws IOException, MlEnginePredictException {
+    final List<Map<String, String>> data = examples.stream().map(example -> {
+      final byte[] bytes = example.toByteArray();
+      final String b64 = BaseEncoding.base64().encode(bytes);
+
+      return Collections.singletonMap("b64", b64);
+    }).collect(Collectors.toList());
+
+    return predict(data);
+  }
+
+  /**
+   * Close the model.
+   */
+  @Override
+  public void close() throws Exception {
+    httpTransport().shutdown();
+  }
+
+  /**
+   * Prediction response.
+   */
+  @AutoValue
+  public abstract static class Response {
+
+    abstract GoogleApiHttpBody content();
+
+    static Response from(final GoogleApiHttpBody content) {
+      return new AutoValue_MlEngineModel_Response(content);
+    }
+
+    /**
+     * List of predictions. Return type depends on the model used.
+     *
+     * @see <a href="https://cloud.google.com/ml-engine/docs/v1/predict-request">https://cloud.google.com/ml-engine/docs/v1/predict-request</a>
+     */
+    public Optional<Predictions> predictions() {
+      final List<Object> predictions = (List<Object>) content()
+          .getOrDefault("predictions", Collections.emptyList());
+
+      if (predictions.isEmpty()) {
+        return Optional.empty();
+      }
+
+      return Optional.of(Predictions.create(predictions));
+    }
+
+    /** Prediction error. */
+    public Optional<String> error() {
+      return Optional.ofNullable((String) content().get("error"));
+    }
+
+    /** Holds the predictions values. */
+    @AutoValue
+    public abstract static class Predictions {
+
+      private static final ObjectMapper MAPPER = new ObjectMapper();
+      private static final Cache<Class<?>, JavaType> CACHE = CacheBuilder.newBuilder().build();
+
+      /**
+       * List of predictions. Return type depends on the model used.
+       *
+       * @see <a href="https://cloud.google.com/ml-engine/docs/v1/predict-request">https://cloud.google.com/ml-engine/docs/v1/predict-request</a>
+       */
+      public abstract List<Object> values();
+
+      /**
+       * List of predictions.
+       *
+       * @param klass class to each returned prediction objects are converted.
+       */
+      public <T> List<T> values(final Class<T> klass) throws ExecutionException {
+        final JavaType javaType =
+            CACHE.get(klass, () -> MAPPER.getTypeFactory().constructType(klass));
+
+        return values()
+            .stream()
+            .map(p -> MAPPER.<T>convertValue(p, javaType))
+            .collect(Collectors.toList());
+      }
+
+      static Predictions create(final List<Object> values) {
+        return new AutoValue_MlEngineModel_Response_Predictions(values);
+      }
+    }
+  }
+
+}
